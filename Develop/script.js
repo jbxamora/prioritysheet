3a3002690bc39f3ed6ed8addb93dd97f569e336e
jbxamora <jbxamora@icloud.com>
2023-01-19 18:44:47 -0800

    expanded function to create new DOM elements dynamically with the help of bootstrap

--- Develop/script.js
+++ Develop/script.js
@@ -10,12 +10,28 @@ var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM" "5PM"];
 function addCurrentDate() {
   currentDay.text(moment().format("MM DD YYYY"));
 }
-
+// Creates Screen Content
 function createText() {
   for (var [i, mHour] of Object.entries(mHours)) {
     var newDiv = $("<div>");
     var textArea = $("<textarea>");
     var button = $("<button>");
-    var timeBlock = $("<span>")
+    var timeBlock = $("<span>");
+
+    // TextArea | TextBlock | Save Button Container
+    newDiv.addClass("row");
+    container.append(newDiv);
+
+    timeBlock.addClass("col-xl-1 time-block hour");
+    timeBlock.text(hours[i]);
+    newDiv.append(timeBlock);
+
+    textArea.addClass(mHour + " col-xl-10 " + hours[i]);
+    newDiv.append(textArea);
+
+    button.addClass("btn btn-primary saveBtn col-xl-1");
+    newDiv.append(button);
+
+
   }
 }
